a1c806c32c078420716bfc05d397fe7445fbb84b
Gaurav Joshi <gaurav9997961651jjk277584@outlook.com>
2026-07-20 08:37:11 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int i=0;
+        int j=1;
+        int maxProfit=0;
+        while(j<prices.size()){
+            if(prices[i]<prices[j]){
+                int profit=prices[j]-prices[i];
+                maxProfit=max(maxProfit,profit);
+            }
+            else i=j;
+            j++;
+        }
+        return maxProfit;
+    }
+};
